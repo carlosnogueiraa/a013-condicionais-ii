fe0377fe51065e84150ebe12c9ef4ce9cb28c590
Carlos Nogueira <carlosnogu8@gmail.com>
2022-08-09 18:03:11 -0300

    Exercício 13

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,23 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número"))
+
+// Questão 1
+    if (num % 2 == 0) {
+        if (num % 3 == 0){
+            console.log("Número divisível por 2 ou 3")
+        } else {
+            console.log("Número divisível por 2")
+        } 
+    } else if (num % 3 == 0){
+        console.log("Número divisível por 3")
+    } else {
+        console.log("Número não divisível por 2 ou 3")
+    }
+
+// Questão 2
+    // if (num % 2 == 0 || num % 3 == 0) {
+    //     console.log("Número divisível por 2 ou 3")
+    // } else {
+    //     console.log("Número não é divisível por 2 ou 3")
+    // }
+
 
